46894b43dd85a657c9e77f3a645c53ed31ae6e67
ms030441 <mark.sun@cerner.com>
2017-10-02 11:07:16 -0400

    Added code to immutability-start

--- src/getStore.js
+++ src/getStore.js
@@ -1,6 +1,12 @@
-import {
-    createStore
-} from 'redux';
+import { createStore} from 'redux';
+import { users } from './../server/db';
+import { getDefaultState } from './../server/getDefaultState';
+import { initializeDB } from './../server/db/initializeDB';
+
+initializeDB();
+
+const currentUser = users[0];
+const defaultState = getDefaultState(currentUser);
 
 const reducer = state => state;
 
